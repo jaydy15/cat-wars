3132eab223f3b7f41525a10b43513b1e815e1dd2
joshua <joshuaadrian46@gmail.com>
2020-08-24 16:17:11 +0800

    Updates

---
+++ precache-manifest.d7412ba507d770a83581524e5d5b0c7c.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fd74730720137f9640540b8ee7cd8a24",
+    "url": "/cat-wars/index.html"
+  },
+  {
+    "revision": "08a401c5546cd84d08f3",
+    "url": "/cat-wars/static/css/main.a3e8802a.chunk.css"
+  },
+  {
+    "revision": "7b555debf535f32f782e",
+    "url": "/cat-wars/static/js/2.9fc7c5d0.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/cat-wars/static/js/2.9fc7c5d0.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "08a401c5546cd84d08f3",
+    "url": "/cat-wars/static/js/main.040c49f8.chunk.js"
+  },
+  {
+    "revision": "ed8a22c2d54800f0fd89",
+    "url": "/cat-wars/static/js/runtime-main.ffa82237.js"
+  }
+]);
